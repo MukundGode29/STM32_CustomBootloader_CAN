2b5891bc2d9a5bea01cb318a2cd3873add6b8712
MukundGode29 <68302166+MukundGode29@users.noreply.github.com>
2026-09-08 09:44:15 +0530

    Add files via upload
    updated Custom bootloader with ota_image in header

---
+++ STM_f103_BootLoader/Core/Core/Inc/bl_jump.h
@@ -0,0 +1,17 @@
+/*
+ * bl_jump.h
+ *
+ *  Created on: 16-Dec-2025
+ *      Author: arunrawat
+ */
+
+#ifndef INC_BL_JUMP_H_
+#define INC_BL_JUMP_H_
+
+#define APP_MAGIC 	0xABCDEFAB
+
+void JumpToApplication(void);
+
+int bootloader_is_app_valid(void);
+
+#endif /* INC_BL_JUMP_H_ */
